63061b376632130bb27c078762cb3fb4f30f22c7
mario-silva-duarte <msduarte@yahoo.com>
2020-06-12 18:38:53 +0100

    AlphaZero

--- heartsTrainerAlpha.py
+++ heartsTrainerAlpha.py
@@ -87,7 +87,7 @@ class HeartsEnv(gym.Env):
                   format(self.hand_points, self.status))
             for p in self.players:
                 print("    Player name {}, Cards {}".
-                      format(p.name, p.cards)
+                      format(p.name, p.cards))
         card = self.players[player_i].play_card(self.status)
         self.players[player_i].cards.remove(card)
         self.status["trick_cards_played"].append(card)
