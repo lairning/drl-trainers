b8a68c41bb7cc08837aac61d58686aace3428b15
mario-silva-duarte <msduarte@yahoo.com>
2021-01-02 20:30:11 +0000

    Testing DQN

--- hearts2021/heartsParametricTrainer.py
+++ hearts2021/heartsParametricTrainer.py
@@ -102,6 +102,7 @@ if __name__ == "__main__":
             "custom_model": "my_model",
         },
         "hiddens": [],
+        "dueling": False,
         #"lr": grid_search([1e-2, 1e-4, 1e-6]),  # try different lrs
         "num_workers": 5,  # parallelism
         "framework": "torch" # if args.torch else "tf",
