4d175d27681f2c263eed7ee056a70409f692aedd
mario-silva-duarte <msduarte@yahoo.com>
2021-01-02 14:45:11 +0000

    Updated tune run config

--- hearts2021/heartAlphaTrainer.py
+++ hearts2021/heartAlphaTrainer.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         "contrib/AlphaZero",
         stop={"training_iteration": 10000},
         max_failures=0,
-        resources_per_trial={"cpu": 2},
+        resources_per_trial={"cpu": 2, "extra_cpu"=2},
         config={
             "env": HeartsAlphaEnv(10),
             "num_workers": 6,
