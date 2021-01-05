79e579e8ed5b2b76278f4f0558f31470bb2664c9
mario-silva-duarte <msduarte@yahoo.com>
2021-01-05 00:02:08 +0000

    Update

--- hearts2021/heartsParametricTrainer.py
+++ hearts2021/heartsParametricTrainer.py
@@ -90,7 +90,7 @@ if __name__ == "__main__":
         },
         "vf_share_layers": False,
         #"lr": grid_search([1e-2, 1e-4, 1e-6]),  # try different lrs
-        "num_workers": 5,  # parallelism
+        "num_workers": 0,  # parallelism
         "framework": "torch" # if args.torch else "tf",
     }
 
