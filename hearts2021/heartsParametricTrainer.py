124036d65db2c7adf845f34b28dd131810184687
mario-silva-duarte <msduarte@yahoo.com>
2021-01-05 00:05:46 +0000

    Update

--- hearts2021/heartsParametricTrainer.py
+++ hearts2021/heartsParametricTrainer.py
@@ -121,7 +121,7 @@ if __name__ == "__main__":
     }
 
     stop = {
-        "training_iteration": 2,
+        "training_iteration": 100,
         "timesteps_total": 200000,
         #"episode_reward_mean": args.stop_reward,
     }
