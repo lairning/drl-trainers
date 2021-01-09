ad9b5d44baf66c65147896d10a596842cd8cfa49
mario-silva-duarte <msduarte@yahoo.com>
2021-01-09 19:31:31 +0000

    Start Embedding Test

--- hearts2021/env.py
+++ hearts2021/env.py
@@ -203,7 +203,7 @@ while not done:
     print("Card {}, Points {}".format(c,points))
 '''
 # TRUE_OBSERVATION_SPACE = Box(0,1,shape=(4*HAND_SIZE,))
-TRUE_OBSERVATION_SPACE = Box(low=0, high=4*HAND_SIZE-1)
+TRUE_OBSERVATION_SPACE = Box(low=0, high=4*HAND_SIZE-1, shape=(1,))
 
 class HeartsParametricEnv:
 
@@ -211,7 +211,7 @@ class HeartsParametricEnv:
         self.env = HeartsEnv0(n_cards)
         self.action_space = Discrete(4*HAND_SIZE)
         self.observation_space = Dict({
-            "obs": Box(low=0, high=4*HAND_SIZE-1), #We are going to create an embbedd
+            "obs": Box(low=0, high=4*HAND_SIZE-1, shape=(1,)), #We are going to create an embbedd
             "action_mask": Box(low=0, high=1, shape=(self.action_space.n, ))
         })
 
