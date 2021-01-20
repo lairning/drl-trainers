4c626d9b738866766650fc53f522ba835733037b
mario-silva-duarte <msduarte@yahoo.com>
2021-01-20 12:47:10 +0000

    Corrections to trainer

--- simpy/trainer.py
+++ simpy/trainer.py
@@ -2,13 +2,14 @@
 import ray
 from ray import tune
 from ray.rllib.agents import dqn, a3c, ppo, sac
+from ray.tune.registry import register_env
 
 import argparse
 
 from simpy_model import SimpyEnv
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--stop", type=int, default=100)
+parser.add_argument("--stop", type=int, default=1)
 
 if __name__ == "__main__":
 
@@ -16,8 +17,13 @@ if __name__ == "__main__":
 
     ray.init()
 
+    register_env(
+        "SimpyEnv",
+        lambda _: SimpyEnv()
+    )
+
     config = {
-        "env_config": {},
+        "env": "SimpyEnv",
         "num_workers": 0
     }
 
@@ -27,7 +33,7 @@ if __name__ == "__main__":
 
     # results_dqn = tune.run(dqn.DQNTrainer, config=dqn_config, stop=stop)
 
-    results = tune.run(ppo.PPOTrainer, env=SimpyEnv, config=config, stop=stop)
+    results = tune.run("PPO", config=config, stop=stop)
 
     # results_sac = tune.run(sac.SACTrainer, config=sac_config, stop=stop)
 
