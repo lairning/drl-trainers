64bb32a317f3193077191d2ac9ccdaf907584c0a
mario-silva-duarte <msduarte@yahoo.com>
2021-02-06 20:07:22 +0000

    Test model config

--- simpy/train_trafic_light.py
+++ simpy/train_trafic_light.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         "v_max": 0,
         "hiddens": [256,256],
         # "noisy": True,
-        "num_atoms": 2,
+        # "num_atoms": 2,
         # "num_workers"  : 5,
         "framework": "torch"
     }
