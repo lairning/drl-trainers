44abbf407532ac7737e8785f8826b3286b712ccb
mario-silva-duarte <msduarte@yahoo.com>
2021-02-06 17:04:33 +0000

    Change model config

--- simpy/train_trafic_light.py
+++ simpy/train_trafic_light.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         "v_max": 0,
         "hiddens": [256,256],
         "noisy": True,
-        "n_atoms": 2
+        "num_atoms": 2
     }
 
     # trainer = ppo.PPOTrainer(config=ppo_config, env="SimpyEnv")
