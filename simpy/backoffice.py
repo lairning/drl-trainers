d728228e8e47fd360ccae755ea4e2d3e1a9cf3ee
mario-silva-duarte <msduarte@yahoo.com>
2021-03-08 12:13:40 +0000

    Debug

--- simpy/backoffice.py
+++ simpy/backoffice.py
@@ -66,11 +66,11 @@ class ModelServer:
         class ServeModel:
             def __init__(self, agent_config: dict, checkpoint_path: str):
                 print(agent_config)
-                assert agent_config is not None and isinstance(agent_config, dict), \
-                    "Invalid Agent Config {} when deploying a policy!".format(agent_config)
+                #assert agent_config is not None and isinstance(agent_config, dict), \
+                #    "Invalid Agent Config {} when deploying a policy!".format(agent_config)
                 print(checkpoint_path)
-                assert checkpoint_path is not None and isinstance(agent_config, str), \
-                    "Invalid Checkpoint Path {} when deploying a policy!".format(checkpoint_path)
+                #assert checkpoint_path is not None and isinstance(agent_config, str), \
+                #    "Invalid Checkpoint Path {} when deploying a policy!".format(checkpoint_path)
                 print("### 1")
                 self.trainer = ppo.PPOTrainer(config=agent_config)
                 print("### 2")
